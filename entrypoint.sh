5c5dfee445285b08635eb99af067eba8f7bc0958
xiaoxi <sisyphusefi@outlook.com>
2023-02-25 17:29:07 +0800

    sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,11 +30,17 @@ else
   ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
 fi
 
-echo 'SFTP Start'
-# create a temporary file containing sftp commands
-printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
-#-o StrictHostKeyChecking=no avoid Host key verification failed.
-sftp -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+if test $10 = "true"; then
+  echo 'SCP Start'
+  #-o StrictHostKeyChecking=no avoid Host key verification failed.
+  scp -P $3 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2:$5 $6
+else
+  echo 'SFTP Start'
+  # create a temporary file containing sftp commands
+  printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
+  #-o StrictHostKeyChecking=no avoid Host key verification failed.
+  sftp -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+fi
 
 echo 'Deploy Success'
 exit 0
