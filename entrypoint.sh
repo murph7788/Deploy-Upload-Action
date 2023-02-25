248ad9608c26d3b968afeb2ecf6c44320613e62b
xiaoxi <sisyphusefi@outlook.com>
2023-02-25 16:18:35 +0800

    sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,12 +28,15 @@ echo '-----debug end-------'
 echo 'ssh start'
 
 # ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
+echo 'sudo ssh -tt -o StrictHostKeyChecking=no -o UserKnownHostsFile=$TEMP_KNOWN_HOST_FILE -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2'
+
 sudo ssh -tt -o StrictHostKeyChecking=no -o UserKnownHostsFile=$TEMP_KNOWN_HOST_FILE -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
 
 echo 'sftp start'
 # create a temporary file containing sftp commands
 printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
 #-o StrictHostKeyChecking=no avoid Host key verification failed.
+echo 'sudo sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2'
 sudo sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
 
 echo 'deploy success'
