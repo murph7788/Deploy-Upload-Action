65c21edb6487a76e2e25d97be0b1a1f6bc8952b2
Murph.Hong <42261069+murph7788@users.noreply.github.com>
2023-02-25 17:04:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,31 +3,38 @@
 #set -e at the top of your script will make the script exit with an error whenever an error occurs (and is not explicitly handled)
 set -eu
 
-
-TEMP_SSH_PRIVATE_KEY_FILE='../private_key'
+TEMP_SSH_PRIVATE_KEY_FILE='../private_key.pem'
 TEMP_SFTP_FILE='../sftp'
-TEMP_KNOWN_HOST_FILE='../hosts'
+
+# make sure remote path is not empty
+if [ -z "$6" ]; then
+   echo 'remote_path is empty'
+   exit 1
+fi
 
 # keep string format
 printf "%s" "$4" >$TEMP_SSH_PRIVATE_KEY_FILE
-# host to hosts file
-printf "%s" "$2" >$TEMP_KNOWN_HOST_FILE
 # avoid Permissions too open
 chmod 600 $TEMP_SSH_PRIVATE_KEY_FILE
 
-echo 'ssh start'
+# delete remote files if needed
+if test $9 == "true";then
+  echo 'Start delete remote files'
+  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 rm -rf $6
+fi
 
-# ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
-echo "ssh -tt -o StrictHostKeyChecking=no -o UserKnownHostsFile=$TEMP_KNOWN_HOST_FILE -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2"
+if test $7 = "true"; then
+  echo "Connection via sftp protocol only, skip the command to create a directory"
+else
+  echo 'Create directory if needed'
+  ssh -o StrictHostKeyChecking=no -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2 mkdir -p $6
+fi
 
-ssh -tt -o StrictHostKeyChecking=no -o UserKnownHostsFile=$TEMP_KNOWN_HOST_FILE -p $3 -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
-
-echo 'sftp start'
+echo 'SFTP Start'
 # create a temporary file containing sftp commands
 printf "%s" "put -r $5 $6" >$TEMP_SFTP_FILE
 #-o StrictHostKeyChecking=no avoid Host key verification failed.
-echo "sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2"
-sftp -b $TEMP_SFTP_FILE -P $3 $7 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
+sftp -b $TEMP_SFTP_FILE -P $3 $8 -o StrictHostKeyChecking=no -i $TEMP_SSH_PRIVATE_KEY_FILE $1@$2
 
-echo 'deploy success'
+echo 'Deploy Success'
 exit 0
